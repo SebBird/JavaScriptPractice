18c4400ff46042caac77448200f46ab6430249f8
Seb <sebbird95@gmail.com>
2021-04-03 13:58:44 +0100

    Lecture practice

---
+++ Functions/DeclarationAndExpression.js
@@ -0,0 +1,11 @@
+// Function Declaration
+// Can be called before it is defined
+function walk() {
+  console.log("walk");
+}
+
+//Function Expression
+// Cannot be called before it is defined
+const run = function () {
+  console.log("run");
+};
